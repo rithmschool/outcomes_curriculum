44ba404cbc459875fdc19b9219a82ee036f75219
Matt Lane <lane.matthew@gmail.com>
2018-09-16 17:34:23 -0700

    fixing bugs in Player class and general cleanup

--- exercises/battleship-oop/solution/models/Player.js
+++ exercises/battleship-oop/solution/models/Player.js
@@ -8,12 +8,11 @@ class Player {
   }
 
   async fireMissile() {
-    let letters = Array.from({ length: Board.__WIDTH }, (_, i) =>
-      String.fromCharCode(97 + i)
-    );
-    let numbers = Array.from({ length: Board.__WIDTH }, (_, i) =>
-      (i + 1).toString()
-    );
+    console.log(`\n${this.name}, it is your turn.`);
+    console.log("Your opponent's board is pictured above.");
+    console.log("Please select where you would like to fire.");
+    let letters = Board.columnLabels;
+    let numbers = Board.rowLabels;
     let columnValidator = this.__inputValidator(letters);
     let rowValidator = this.__inputValidator(numbers);
     let column = await promptly.prompt(
@@ -28,46 +27,51 @@ class Player {
       }`,
       { validator: rowValidator }
     );
-    return [numbers.indexOf(row), letters.indexOf(column)];
+    return [letters.indexOf(column), numbers.indexOf(row)];
   }
 
   checkForHit(coords) {
-    let [x, y] = coords;
-    this.board.view[x][y] = "M";
-    this.board.ships.forEach(ship => {
+    let [ltr, num] = coords;
+    let hitIdx;
+    for (let ship of this.board.ships) {
       let hitHorizontalShip =
         ship.orientation === "horizontal" &&
-        x === ship.startX &&
-        y >= ship.startY &&
-        y <= ship.endY;
+        num === ship.startY &&
+        ltr >= ship.startX &&
+        ltr <= ship.endX;
       let hitVerticalShip =
         ship.orientation === "vertical" &&
-        y === ship.startY &&
-        x >= ship.startX &&
-        x <= ship.endX;
-      let hitIdx;
-      if (hitHorizontalShip) hitIdx = y - ship.startY;
-      else if (hitVerticalShip) hitIdx = x - ship.startX;
-      if (hitIdx) {
-        this.board.view[x][y] = "H";
+        ltr === ship.startX &&
+        num >= ship.startY &&
+        num <= ship.endY;
+      if (hitHorizontalShip) hitIdx = ltr - ship.startX;
+      else if (hitVerticalShip) hitIdx = num - ship.startY;
+      if (hitIdx !== undefined) {
+        console.log("You hit a ship!");
+        this.board.view[num][ltr] = "H";
         ship.recordHit(hitIdx);
         if (ship.isSunk()) {
           console.log(`You sunk a ship of length ${ship.length}!`);
         }
+        return;
       }
-    });
+    }
+    console.log("You missed!");
+    this.board.view[num][ltr] = "M";
   }
 
   hasLost() {
     return this.board.ships.every(ship => ship.isSunk());
   }
 
-  __inputValidator(data, input) {
-    let lcInput = input.toLowerCase();
-    if (!data.includes(lcInput)) {
-      throw new Error(`Please enter a value in ${data}.`);
-    }
-    return lcInput;
+  __inputValidator(data) {
+    return function(input) {
+      let ucInput = input.toUpperCase();
+      if (!data.includes(ucInput)) {
+        throw new Error(`Please enter a value in ${data}.`);
+      }
+      return ucInput;
+    };
   }
 }
 
